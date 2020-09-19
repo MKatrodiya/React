f8f2583f6006fc2c035f5dd7873e4b298435a182
Meet Katrodiya <meetkatrodiya@icloud.com>
2020-09-19 11:51:48 +0530

    React routers part-2

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
     function RenderMenuItem({ dish, onClick }) {
         return(
-            <Card onClick={() => onClick(dish.id)}>
-                <CardImg width="100%" src={dish.image} alt={dish.name} />
-                <CardImgOverlay>
-                    <CardTitle>{dish.name}</CardTitle>
-                </CardImgOverlay>
+            <Card>
+                <Link to={`/menu/${dish.id}`} >
+                    <CardImg width="100%" src={dish.image} alt={dish.name} />
+                    <CardImgOverlay>
+                        <CardTitle>{dish.name}</CardTitle>
+                    </CardImgOverlay>
+                </Link>
             </Card>    
         );
     }
@@ -15,7 +18,7 @@ import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 're
     const Menu = (props) => {
         const menu = props.dishes.map((dish) => (
             <div key={dish.id} className="col-12 col-md-5 mt-1">
-                <RenderMenuItem dish={dish} onClick={props.onClick} />
+                <RenderMenuItem dish={dish} />
             </div>
         ));
 
@@ -23,6 +26,16 @@ import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 're
 
         return(
             <div className="container">
+                <div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to='/home'>Home</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>Menu</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>Menu</h3>
+                        <hr/>
+                    </div>
+                </div>
                 <div className="row">
                     {menu}
                 </div>
